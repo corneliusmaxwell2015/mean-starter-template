d2d9b59b51f87632c384522506131a6033537e31
Cornelius Maxwell <156154530+corneliusmaxwell2015@users.noreply.github.com>
2026-06-09 18:54:06 -0500

    Refactor AppComponent to use inline template
    Replaced templateUrl and styleUrls with inline template.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<h1>Hello from Angular Client</h1>`
 })
-export class AppComponent {
-  title = 'mean-starter';
-}
+export class AppComponent {}
