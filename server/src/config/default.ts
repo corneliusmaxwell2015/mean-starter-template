476d7c448fa0da58be521fdd3abbce8c2a93856f
Cornelius Maxwell <156154530+corneliusmaxwell2015@users.noreply.github.com>
2026-06-09 00:58:49 -0500

    Update default.ts

--- server/src/config/default.ts
+++ server/src/config/default.ts
@@ -1,4 +1,6 @@
 export default {
   port: 5000,
-  mongoUri: "mongodb://localhost:27017/mean-starter"
+  mongoUri: "mongodb://localhost:27017/mean-starter",
+  jwtSecret: "changeme",
+  jwtRefreshSecret: "changemerefresh"
 };
