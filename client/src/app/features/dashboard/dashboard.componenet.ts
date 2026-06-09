8af2c2a28e621e3b6105dfc2d54f74a2b7bfb5ce
Cornelius Maxwell <156154530+corneliusmaxwell2015@users.noreply.github.com>
2026-06-09 16:25:23 -0500

    Create dashboard.componenet.ts

---
+++ client/src/app/features/dashboard/dashboard.componenet.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
